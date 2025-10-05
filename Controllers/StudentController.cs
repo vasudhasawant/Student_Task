523c7c7507b09f29a4dc1b4d32fe6a116606488c
Vasudha <Vasudha181202@gmail.com>
2025-10-05 10:05:19 +0530

    Initial commit for StudentTask project

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using StudentTask.Models;
+
+namespace StudentTask.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (_context.Students.Any(s => s.Email == student.Email))
+            {
+                ModelState.AddModelError("Email", "Email Already Exists!");
+                return View(student);
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.Find(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id,Student student)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                _context.Students.Update(student);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentTask.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Course { get; set; } = null!;
+
+    public int Marks { get; set; }
+}
